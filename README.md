3a7740751aa4cf4646865445064058dc2b1f1fa6
sjaehn <kontakt@jahnichen.de>
2023-03-23 13:23:45 +0100

    What's sooo bad about GTK?

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ Programming tutorial series for creating LV2 plugins using C/C++ and turtle.
   - [01 - Introduction Into LV2 UIs](#01---introduction-into-lv2-uis)
   - [02 - A Simple Plugin UI Using GTK (Part 1)](#02---a-simple-plugin-ui-using-gtk-part-1)
   - [03 - A Simple Plugin UI Using GTK (Part 2)](#03---a-simple-plugin-ui-using-gtk-part-2)
+  - [04 - What's Sooo Bad About GTK?](#04---whats-sooo-bad-about-gtk)
+  
 - [Resources](#resources)
 - [Further reading](#further-reading)
 
@@ -381,6 +383,47 @@ Build:
 myAmp_GTK3.lv2) of your lv2 directory.
 
 GTK3: https://docs.gtk.org/gtk3/
+
+
+### 04 - What's Sooo Bad About GTK?
+
+See video: Friday, 2023/03/24, 14:00 CET
+
+No, GTK is not bad! But the use within plugins is not recommended. And this
+is also true for other big GUI toolkits. Like Qt.
+
+Our GTK3-based Plugin UI doesn't work in Ardour. Why?
+
+On the other hand, if we make a GTK(2)-based version using `ui:GtkUI`, and compile it by:
+```
+g++ myAmp_GTK.c -fvisibility=hidden -fPIC -DPIC -shared -pthread -lm `pkg-config --cflags --libs lv2 gtk+-2.0` -o myAmp_GTK.so
+```
+Now it runs in Ardour. But it fails in jalv.gtk3. Why?
+
+
+Although LV2 contains specifications for GTK(2) and GTK3,
+* GTK3 plugin UIs don't work in GTK2 LV2 hosts (javl.gtk, Ardour)
+* GTK2 plugin UIs may fail in GTK3 LV2 hosts (jalv.gkt3, Zrythm)
+* any GTK-based plugins don't work in host which don't support GTK(2)/3 
+(Reaper).
+
+The reason: linking. There are two types of linking:
+* static linking (code included, at compile time)
+* dynamic linking (shared objects, at runtime)
+
+You can't dynamicly link to two different versions of the same library. And
+you don't have any control about the library loaded before.
+
+The solution: static linking (as much as possible). (Or provide UIs for all 
+toolkits. But I wouldn't do all the work ;-)). But GTK, Qt and other big UI
+toolkits are made for dynamic linking. And they are full of compatibility
+breaks. Use UI toolkits which can be staticly linked, instead.
+
+**Note:** The linking process is shown in a much simplified way. There a more
+steps than shown in this video. Including relocation, symbol table, symbol
+resolution, ... . But this would be far beyond this tutorial series. 
+
+And there are also different ways of loading plugins by the host.
 
 
 ## Resources
---
+++ myAmp_GTK/myAmp_GTK.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include "myAmp_GTK.hpp"
+#include <stdexcept>
+
+MyAmpUI::MyAmpUI (LV2UI_Write_Function write_function, LV2UI_Controller controller):
+    write_function(write_function),
+    controller(controller)
+{
+    box = gtk_vbox_new (false, 0);
+    slider = gtk_vscale_new_with_range (0.0, 2.0, 0.01);
+    gtk_range_set_value (reinterpret_cast<GtkRange*>(slider), 1.0);
+    g_signal_connect_after (slider, "value-changed", G_CALLBACK(valueChangedCallback), this);
+    gtk_container_add (reinterpret_cast<GtkContainer*>(box), slider);
+    gtk_widget_set_size_request (box, 50, 200);
+}
+
+LV2UI_Widget MyAmpUI::getMain()
+{
+    return static_cast<LV2UI_Widget>(box);
+}
+
+void MyAmpUI::portEvent(uint32_t port_index, uint32_t buffer_size, uint32_t format, const void *buffer)
+{
+    if (format == 0)
+    {
+        const float value = *static_cast<const float*>(buffer);
+        switch (port_index)
+        {
+            case 2:     gtk_range_set_value (reinterpret_cast<GtkRange*>(slider), value);
+                        break;
+
+            default:    break;
+        }
+    }
+}
+
+int MyAmpUI::valueChangedCallback (GtkWidget* widget, gpointer data)
+{
+    MyAmpUI* ui = static_cast<MyAmpUI*>(data);
+    if (ui)
+    {
+        float gain = gtk_range_get_value (reinterpret_cast<GtkRange*>(widget));
+        ui->write_function (ui->controller, 2, sizeof(gain), 0, &gain);
+    }
+
+    return 0;
+}
+
+static LV2UI_Handle instantiate(const struct LV2UI_Descriptor *descriptor, const char *plugin_uri, const char *bundle_path, LV2UI_Write_Function write_function, LV2UI_Controller controller, LV2UI_Widget *widget, const LV2_Feature *const *features)
+{
+    if (strcmp (plugin_uri, "https://github.com/sjaehn/lv2tutorial/myAmp_GTK") != 0) return nullptr;
+    MyAmpUI* ui;
+    try {ui = new MyAmpUI(write_function, controller);}
+    catch (std::exception& exc)
+    {
+        std::cerr << "UI instantiation failed." << std::endl;
+        return nullptr;
+    }
+
+    *widget = ui->getMain();
+    return (LV2UI_Handle) ui;
+}
+
+static void cleanup (LV2UI_Handle ui)
+{
+    MyAmpUI* myUi = static_cast<MyAmpUI*>(ui);
+    if (myUi) delete myUi;
+}
+
+static void port_event(LV2UI_Handle ui, uint32_t port_index, uint32_t buffer_size, uint32_t format, const void *buffer)
+{
+    MyAmpUI* myUi = static_cast<MyAmpUI*>(ui);
+    if (myUi) myUi->portEvent(port_index, buffer_size, format, buffer);
+}
+
+static const void * extension_data (const char *uri)
+{
+    return nullptr;
+}
+
+static const LV2UI_Descriptor ui_descriptor =
+{
+    "https://github.com/sjaehn/lv2tutorial/myAmp_GTK#ui",
+    instantiate,
+    cleanup,
+    port_event,
+    extension_data
+};
+
+LV2_SYMBOL_EXPORT const LV2UI_Descriptor * 	lv2ui_descriptor (uint32_t index)
+{
+    switch (index)
+    {
+        case 0:     return &ui_descriptor;
+        default:    return 0;
+    }
+}
+
