037a85bea51d18196e37386f8135afd7a852c37a
sjaehn <kontakt@jahnichen.de>
2023-04-13 22:39:22 +0200

    ... but BMap still on  GPL3+

--- mySimplePolySynth/BMap.hpp
+++ mySimplePolySynth/BMap.hpp
@@ -1,3 +1,20 @@
+/* BMap.hpp
+ * Copyright (C) 2021  Sven Jähnichen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef BUTILITIES_BMAP_HPP_
 #define BUTILITIES_BMAP_HPP_
 
